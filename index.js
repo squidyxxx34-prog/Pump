13de061b1f4583f7e41f603dae1cedab89ce3d05
squidyxxx34-prog <squidyxxx34@gmail.com>
2026-08-12 19:48:13 -0700

    Update index.js

--- index.js
+++ index.js
@@ -313,7 +313,7 @@ http
   })
   .listen(port, () => log(`Healthcheck HTTP en écoute sur le port ${port}`));
 
-// ---------- Arrêt propre + robustesse ----------
+// ---------- Arrêt propre + robustessejb ----------
 // évite un crash bruyant (npm error) sur un SIGTERM normal de Railway (redeploy, scaling)
 function gracefulShutdown(signal) {
   log(`Signal ${signal} reçu, arrêt propre du bot...`);
